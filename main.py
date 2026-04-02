b03d63d1a460523faa627a60170b9b3cb24046f8
shreya29999 <shreya29999-shreya.singh@shilshatech.com>
2026-04-02 17:50:52 +0530

    added cors

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ app = FastAPI(
 # CORS for frontend dev
 app.add_middleware(
     CORSMiddleware,
-    allow_origins     = ["*"],
+    #allow_origins     = ["*"],
+    allow_origins     = ["*","http://localhost:3000"],
     allow_credentials = True,
     allow_methods     = ["*"],
     allow_headers     = ["*"],
